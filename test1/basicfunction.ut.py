a899a8772bfcb7012995ecc2b47332e371d7ab0a
marundu <marundu@gmail.com>
2018-03-19 09:59:45 +0300

    Added test case to illustrate verbose output

--- test1/basicfunction.ut.py
+++ test1/basicfunction.ut.py
@@ -1,10 +1,14 @@
 # Does not check whether basicfunction.py passes
+# $ python basicfunction.ut.py -v (verbose output)
 
 import unittest
 
 class TestBasicFunction(unittest.TestCase):
-    def test(self):
+    def test1(self):
         self.assertTrue(True) # will always pass in this case
+    
+    def test2(self):
+        self.assertTrue(True)
 
 if __name__=='__main__':
     unittest.main()
